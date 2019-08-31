0bdc628664593e1d3e380ed81f1d794653b21fca
xpy-110 <1102736593@qq.com>
2019-08-31 09:12:55 +0800

    xpy

--- src/main/java/com/qf/travel/pojo/Numbb.java
+++ src/main/java/com/qf/travel/pojo/Numbb.java
@@ -3,16 +3,19 @@ package com.qf.travel.pojo;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class Numbb implements Serializable {
     private String name;
-    private int value;
+    private List<NumEcharts> list = new ArrayList<>();
 
     public Numbb() {
     }
 
-    public Numbb(String name, int value) {
+    public Numbb(String name, List<NumEcharts> list) {
         this.name = name;
-        this.value = value;
+        this.list = list;
     }
 }
